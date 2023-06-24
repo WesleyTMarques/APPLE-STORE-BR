8e25bec3ed5c4b21ec57438aef65837135efc608
WesleyTMarques <wesleytmarques@outlook.com>
2023-06-23 22:07:49 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaIphone(iphone){
+    let imgIphone = document.querySelector(".foto-iphone")
+    imgIphone.src = iphone
+}
